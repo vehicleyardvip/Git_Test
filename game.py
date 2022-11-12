f006debb6b536c5bf291c7120b5e809cbfc56d6e
Vehicle Yard <vehicleyard@163.com>
2022-11-12 21:22:45 +0800

    家和公司合并版本

--- game.py
+++ game.py
@@ -1,20 +1,28 @@
 import random
 
 print('------------------我爱鱼C工作室------------------')
-secret = random.randint(1,10)
-temp = input("不妨猜一下小甲鱼现在心里想的是哪个数字：")
-guess = int(temp)
 
-while guess != secret:
-    temp = input("哎呀，猜错了，请重新输入吧：")
+times = 3
+secret = random.randint(1,10)
+guess = 0
+
+print("不妨猜一下小甲鱼现在心里想的是哪个数字：", end=" ")
+
+while (guess != secret) and (times > 0):
+    temp = input()
     guess = int(temp)
+    times = times - 1 # 用户每输入一次，可用机会就-1
     if guess == secret:
-        print("卧槽，你是小甲鱼心里的蛔虫吗？！")
+        print("哇哦，你是小甲鱼心里的蛔虫吗？！")
         print("哼，猜中了也没有奖励！")
     else:
         if guess > secret:
-            print("大了，大了~~~")
+            print("哥，大了大了~~~")
         else:
-            print("小了，小了~~~")
+            print("嘿，小了，小了~~~")
+        if times > 0:
+            print("再试一次吧：", end=" ")
+        else:
+            print("机会用光咯T_T")
             
 print("游戏结束，不玩啦^_^")
